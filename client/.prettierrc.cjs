412741f3d4316aa62f89ea249263c4137415d848
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-11-13 22:03:25 +0100

    made typescript, eslint and prettier work in the monorepo

---
+++ client/.prettierrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  // organizeImportsSkipDestructiveCodeActions: true,
+  plugins: [require.resolve('prettier-plugin-organize-imports')],
+}
